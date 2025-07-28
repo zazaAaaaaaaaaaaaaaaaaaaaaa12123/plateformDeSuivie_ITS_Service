08e67236881311ceb6ee302d265f73630dad865b
Mohamed Batiene <batienemohamed330@gmail.com>
2025-07-28 18:58:18 +0000

    amelioration ...

--- serverITS.js
+++ serverITS.js
@@ -3363,7 +3363,7 @@ app.get("/html/repoLivAuth.html", (req, res) => {
 // ===============================
 // ROUTE CATCH-ALL POUR SERVIR LE FRONTEND (index.html)
 // ===============================
-// Cette route doit être TOUT EN BAS, après toutes les routes API !
+// Cette route doit être TOUT EN BAS, après toutes les routes API !!
 // (Le static public est déjà défini plus haut, mais on s'assure que la route / est bien la dernière)
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "html", "index.html"));
