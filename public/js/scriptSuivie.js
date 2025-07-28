8beca56ea469128b298c57e1e0612066eaee81c5
Mohamed Batiene <batienemohamed330@gmail.com>
2025-07-28 16:48:00 +0000

    SFDHGSVSGVJHD?

--- public/js/scriptSuivie.js
+++ public/js/scriptSuivie.js
@@ -8978,6 +8978,6 @@ if (window["WebSocket"]) {
     originalApplyCombinedFilters.apply(this, args);
     setTimeout(forceBlinkOnNewRows, 50); // Laisse le DOM se mettre à jour
   };
-  // ================shgj FIN CLIGNOTEMENT VERT ==================
+  // ================shg FIN CLIGNOTEMENT VERT ==================
 })();
 /****** Script a ajouter en cas de pertubation 125 GGGAAAA34 ***/
